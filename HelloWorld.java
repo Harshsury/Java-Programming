2d09e3da00dec1a588d4ee4710f3aac3f43f6cef
Harshsury <harshsuryawanshi666@gmail.com>
2025-09-21 17:56:26 +0530

    first java programming

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Everyone");
+        System.out.println("My name is Ruchi Suryavanshi, i am softare developer");
+    }
+}
